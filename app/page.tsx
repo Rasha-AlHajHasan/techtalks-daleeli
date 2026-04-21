95acd73b17c82c128d489b9ba340a5dcc9e9b338
ahmadalbourhan <127745237+ahmadalbourhan@users.noreply.github.com>
2026-04-21 22:18:47 +0300

    refactor: remove supabase data fetching and logging from Page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,4 @@
-import { supabase } from "./lib/supabase/client";
-
 export default async function Page() {
-  const { data, error } = await supabase.from("test").select("*");
-
-  console.log("DATA:", data);
-  console.log("ERROR:", error);
-
   return (
     <div>
       <h1>Check console</h1>
