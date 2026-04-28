3e29294c5bbc01610156ed77448238c6da94faf4
adhambatlouni <adhamalbatlouni@gmail.com>
2026-04-28 11:54:30 +0300

    feat(i18n): add routing, request, and navigation configuration files

---
+++ i18n/routing.ts
@@ -0,0 +1,9 @@
+// i18n/routing.ts
+import { defineRouting } from "next-intl/routing";
+
+export const routing = defineRouting({
+  locales: ["en", "fr", "ar"],
+  defaultLocale: "en",
+  localePrefix: "never", 
+  localeDetection: true, 
+});
